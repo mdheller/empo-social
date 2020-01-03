fd26fbd0033c36ea4440bdf0f3db274ce9287c4d
VanVan <s.sieuux@gmail.com>
2020-01-03 16:23:37 +0700

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
-//export const API_ENDPOINT = "https://api-testnet.empow.io"
-
-export const API_ENDPOINT = "http://localhost:5000"
-
-export const SOCKET = "http://localhost:8000"
+export const API_ENDPOINT = "https://api-testnet.empow.io"
+export const SOCKET = "https://socket-testnet.empow.io"
+//export const API_ENDPOINT = "http://localhost:5000"
+//export const SOCKET = "http://localhost:8000"
